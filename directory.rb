6e4105af5c6044bf874237765c41733c6286ad05
Jake Atkin <jakeatkin@Ashleighs-MacBook-Air.local>
2019-01-04 12:58:43 +0000

    Saving data

--- directory.rb
+++ directory.rb
@@ -7,9 +7,12 @@ def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
 
+  # get the first name
   name = gets.chomp
 
+  # while the name is not empty, repeat this code
   until name.empty? do
+    # add the student hash to the array
     puts "Please enter a hobby:"
     hobby = gets.chomp
     puts "Please enter a cohort"
@@ -21,6 +24,7 @@ def input_students
 
     @students << {name: name, cohort: cohort.to_sym, hobby: hobby}
     puts "Now we have #{@students.count} students"
+    # get another name from the user
     puts "Please enter a name:"
     name = gets.chomp
   end
@@ -75,7 +79,8 @@ end
 def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
-  puts "9. Exit"
+  puts "3. Save the list to students.csv"
+  puts "9. Exit" # 9 because we'll be adding more items
 end
 
 def show_students
@@ -84,14 +89,26 @@ def show_students
   print_footer
 end
 
+def save_students
+  file = File.open("students.csv", "w")
+  @students.each do |student|
+    student_data = [student[:name], student[:cohort]]
+    csv_line = student_data.join(",")
+    file.puts csv_line
+  end
+  file.close
+end
+
 def process(selection)
   case selection
     when "1"
-    input_students
+      input_students
     when "2"
-    show_students
+      show_students
+    when "3"
+      save_students
     when "9"
-    exit
+      exit # this will cause the program to terminate
     else
     puts "I don't know what you meant, try again"
   end
